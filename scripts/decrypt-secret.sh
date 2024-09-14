6df832af543ff0b57bdd563553d2d671ab846040
Dan Blair <s223215521@deakin.edu.au>
2024-09-14 18:45:52 +1000

    Updated kubeconfig secret

--- scripts/decrypt-secret.sh
+++ scripts/decrypt-secret.sh
@@ -4,5 +4,7 @@
 mkdir -p $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output - kubeconfig.txt.gpg | base64 -d > $HOME/secrets/kubeconfig.txt
+# gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+# --output - kubeconfig.txt.gpg | base64 -d > $HOME/secrets/kubeconfig.txt
+
+cp kubeconfig.txt $HOME/secrets/kubeconfig.txt
