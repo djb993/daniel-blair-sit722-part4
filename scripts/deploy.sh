9c9e0e442d1a8421bac852f978a531f92e2c0251
Dan Blair <s223215521@deakin.edu.au>
2024-09-14 19:41:15 +1000

    Updated kubeconfig

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,16 +12,17 @@ set -euo pipefail
 
 echo -e "\n${p}Deploying Microservices...${d}\n"
 
+cat $HOME/secrets/kubeconfig.txt
+
 # Ensure KUBECONFIG is set
 export KUBECONFIG="$HOME/secrets/kubeconfig.txt"
 
 # Debugging
 echo "KUBECONFIG is set to: $KUBECONFIG"
-cat "$KUBECONFIG"
 
 envsubst < ./scripts/kubernetes/deployment.yaml | kubectl apply -f -
 
 echo -e "\n${p}Deployment complete.${d}\n"
 
 # Display Status & Launch Browser
-chmod +x ./scripts/display-status-launch-browser.sh
+./scripts/display-status-launch-browser.sh
