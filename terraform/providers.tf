3d8e3fb0e60a7baf0caf6c5c827d7b58b2057772
djb993 <141124087+djb993@users.noreply.github.com>
2024-09-28 17:13:43 +1000

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -13,4 +13,8 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  subscription_id = var.azure_subscription_id
+  client_id      = var.azure_client_id
+  tenant_id      = var.azure_tenant_id
 }
